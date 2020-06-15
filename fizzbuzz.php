316d2383b7b4926f144f3f1ea9af6ab7f34ae113
takuya tamada <tmdtky@gmail.com>
2020-06-15 19:53:22 +0900

    first commit

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,8 +1,11 @@
 <?php
+
     for($i = 1; $i <= 150; $i++) {
-        if($i % 3 == 0 || mb_substr($i, -1) == 3) {
-            echo $i."!\n";
-        } else {
+        if ($i >= 100) {
             echo $i."\n";
+        } elseif ($i % 3 == 0 || mb_substr($i, -1) == 3) {
+            echo sprintf("%02d!\n", $i);
+        } else {
+            echo sprintf("%03d\n", $i);
         }
     }
